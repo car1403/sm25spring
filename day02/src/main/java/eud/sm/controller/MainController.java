7100080c246b5d3846929f662c252bb654e4d53f
jmlee <jmlee@tonesol.com>
2025-07-10 10:50:07 +0900

    0710

---
+++ day02/src/main/java/eud/sm/controller/MainController.java
@@ -0,0 +1,18 @@
+package eud.sm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    // https://127.0.0.1/
+    @RequestMapping("/")
+    public String main(Model model) {
+        return "index";
+    }
+
+}
